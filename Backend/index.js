d42c4cc9d13eade242b656cf9cf1d23cc904c685
JamesG <1869237+shinygit@users.noreply.github.com>
2019-12-30 14:41:25 -0600

    authentication JWT backend

--- Backend/index.js
+++ Backend/index.js
@@ -1,8 +1,9 @@
 const express = require('express')
 const cors = require('cors')
-
+const passport = require('passport')
 const db = require('./database')
 const itemRouter = require('./routes/item-router')
+const userRouter = require('./routes/users')
 
 const app = express()
 const apiPort = 3001
@@ -12,10 +13,13 @@ app.use(express.json())
 
 db.on('error', console.error.bind(console, 'MongoDB connection error:'))
 
+app.use(passport.initialize())
+require('./authentication/passport')(passport)
+
 app.get('/', (req, res) => {
   res.send('Hello World!')
 })
-
+app.use('/api/users', userRouter)
 app.use('/api', itemRouter)
 
 app.listen(apiPort, () => console.log(`Server running on port ${apiPort}`))
---
+++ Frontend/src/components/NavBar/NavBar.js
@@ -0,0 +1,11 @@
+import React from 'react'
+const NavBar = () => {
+  return (
+    <div>
+      <button>Login</button>
+      <button>Register</button>
+    </div>
+  )
+}
+
+export default NavBar
