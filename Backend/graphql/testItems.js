65b2febe0e83c3af28bd451b97a409d0af6555a6
JamesJ <test>
2020-01-12 12:23:34 -0600

    some graphql progress

---
+++ Backend/graphql/testItems.js
@@ -0,0 +1,30 @@
+const items = {
+  1: {
+    id: 1,
+    itemName: 'test item 1',
+    supplier: 'test supplier 1',
+    location: 'test location 1',
+    buildTo: 1,
+    order: 0,
+    showEditForm: false,
+    isLocked: false,
+    submittedDate: Date.now(),
+    itemID: '239n2903h42340',
+    user: 'Ch'
+  },
+  2: {
+    id: 2,
+    itemName: 'test item 2',
+    supplier: 'test supplier 2',
+    location: 'test location 2',
+    buildTo: 2,
+    order: 0,
+    showEditForm: false,
+    isLocked: false,
+    submittedDate: Date.now(),
+    itemID: '2asdf03h423vb',
+    user: 'Ch'
+  }
+}
+
+module.exports = items
--- Backend/index.js
+++ Backend/index.js
@@ -1,25 +1,19 @@
-const express = require('express')
+require('dotenv/config')
 const cors = require('cors')
-const passport = require('passport')
-const db = require('./database')
-const itemRouter = require('./routes/item-router')
-const userRouter = require('./routes/users')
+const express = require('express')
+const { ApolloServer, gql } = require('apollo-server-express')
+
+const typeDefs = require('./graphql/schema.js')
+const resolvers = require('./graphql/resolvers.js')
 
 const app = express()
-const apiPort = 3001
-app.use(express.urlencoded({ extended: true }))
 app.use(cors())
-app.use(express.json())
 
-db.on('error', console.error.bind(console, 'MongoDB connection error:'))
-
-app.use(passport.initialize())
-require('./authentication/passport')(passport)
-
-app.get('/', (req, res) => {
-  res.send('Hello World!')
+const server = new ApolloServer({
+  typeDefs,
+  resolvers
 })
-app.use('/api/users', userRouter)
-app.use('/api', passport.authenticate('jwt', { session: false }), itemRouter)
 
-app.listen(apiPort, () => console.log(`Server running on port ${apiPort}`))
+server.applyMiddleware({ app, path: '/graphql' })
+
+app.listen(3001, () => console.log(`Server running on port 3001`))
