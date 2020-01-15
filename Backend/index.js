92471aad1f18da901bfeccdd7ab593683de2f37c
JamesJ <test>
2020-01-14 19:13:32 -0600

    more...

--- Backend/index.js
+++ Backend/index.js
@@ -1,4 +1,3 @@
-require('dotenv/config')
 const cors = require('cors')
 const express = require('express')
 const { ApolloServer } = require('apollo-server-express')
@@ -13,13 +12,52 @@ app.use(cors())
 const server = new ApolloServer({
   typeDefs: schema,
   resolvers,
-  context: {
+  context: async () => ({
     models,
-    me: models.users[1]
-  }
+    me: await models.User.findByLogin('rwieruch')
+  })
 })
 
 server.applyMiddleware({ app, path: '/graphql' })
-sequelize.sync().then(async () => {
-  app.listen({ port: 3001 }, () => console.log(`Server running on port 3001`))
+
+const eraseDatabaseOnSync = true
+sequelize.sync({ force: eraseDatabaseOnSync }).then(async () => {
+  if (eraseDatabaseOnSync) {
+    createUsersWithMessages()
+  }
+  app.listen({ port: 3001 }, () => {
+    console.log('Apollo Server on http://localhost:3001/graphql')
+  })
 })
+
+const createUsersWithMessages = async () => {
+  await models.User.create(
+    {
+      username: 'rwieruch',
+      items: [
+        {
+          itemName: 'Published the Road to learn React'
+        }
+      ]
+    },
+    {
+      include: [models.Item]
+    }
+  )
+  await models.User.create(
+    {
+      username: 'ddavids',
+      items: [
+        {
+          itemName: 'Happy to release ...'
+        },
+        {
+          itemName: 'Published a complete ...'
+        }
+      ]
+    },
+    {
+      include: [models.Item]
+    }
+  )
+}
