c3284d1543126fb5ed338d4aafa11f5819b1be28
JamesG <1869237+shinygit@users.noreply.github.com>
2020-03-05 12:32:42 -0600

    fixed token check

--- Backend/index.js
+++ Backend/index.js
@@ -18,7 +18,7 @@ app.use(cors())
 
 const getMe = async req => {
   const token = req.headers['x-token']
-  if (token !== '') {
+  if (token) {
     try {
       return await jwt.verify(token, process.env.SECRET)
     } catch (e) {
