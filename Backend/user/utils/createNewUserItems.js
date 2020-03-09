63b2f455355fd942a728efa5ea332f511fefd35d
JamesJ <1869237+shinygit@users.noreply.github.com>
2020-03-09 18:54:29 -0500

    new user default items contain new fields

--- Backend/user/utils/createNewUserItems.js
+++ Backend/user/utils/createNewUserItems.js
@@ -1,7 +1,29 @@
 export const createUserStarterOrderAndItems = async (user, models) => {
   const order = await models.Order.create({ orderDate: Date.now() })
-  const item1 = await models.Item.create({ itemName: 'Milk', buildTo: 2 })
-  const item2 = await models.Item.create({ itemName: 'Sugar', buildTo: 2 })
+  const item1 = await models.Item.create({
+    itemName: 'Milk',
+    buildTo: 2,
+    quanityOnHand: 1,
+    unitPriceInPennies: 245,
+    isMarketPrice: false,
+    productNumber: '9u12012h',
+    unitSize: '1 Gallon',
+    quantityReceived: 0,
+    itemNote: 'Check expiration date!',
+    specialNote: 'Check the price on chocolate milk.'
+  })
+  const item2 = await models.Item.create({
+    itemName: 'Sugar',
+    buildTo: 2,
+    quanityOnHand: 1,
+    unitPriceInPennies: 45,
+    isMarketPrice: false,
+    productNumber: '129837-vx',
+    unitSize: '1 Cup',
+    quantityReceived: 0,
+    itemNote: "Make sure it's always cane.",
+    specialNote: ''
+  })
   const loc1 = await models.Location.create({ locationName: 'Fridge' })
   const loc2 = await models.Location.create({ locationName: 'Pantry' })
   const sup1 = await models.Supplier.create({
