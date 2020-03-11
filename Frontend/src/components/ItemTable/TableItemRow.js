55e12306c01098269c84605ad0faad55547622df
JamesJ <1869237+shinygit@users.noreply.github.com>
2020-03-11 12:36:06 -0500

    extracted some icons

--- Frontend/src/components/ItemTable/TableItemRow.js
+++ Frontend/src/components/ItemTable/TableItemRow.js
@@ -1,43 +1,45 @@
-import React from 'react'
+import React, { useState } from 'react'
 import ChangeOrderAmount from './ChangeOrderAmount'
+import TableItemRowExpanded from './TableItemRowExpanded'
+import ClipboardDown from './icons/ClipboardDown'
+import ClipboardUp from './icons/ClipboardUp'
 
 const areEqual = (prevProps, nextProps) => {
   return prevProps.item === nextProps.item
 }
 const TableItemRow = React.memo(({ item, handleEdit }) => {
+  const [expanded, setExpanded] = useState(false)
   return (
-    <tr>
-      <td className='hidden md:table-cell'>
-        <button onClick={() => handleEdit(item.id)}>
-          <svg
-            xmlns='http://www.w3.org/2000/svg'
-            className='h-5 w-5'
-            viewBox='0 0 20 20'
-          >
-            <path d='M12.3 3.7l4 4L4 20H0v-4L12.3 3.7zm1.4-1.4L16 0l4 4-2.3 2.3-4-4z' />
-          </svg>
-        </button>
-      </td>
-      <td className='border border-gray-700 text-center px-1'>
-        {item.itemName}
-      </td>
-      <td className='border border-gray-700 text-center'>{item.buildTo}</td>
-      <td className='hidden md:table-cell border border-gray-700 text-center'>
-        {item.previousOrders[1]}
-      </td>
-      <td className='border border-gray-700 text-center'>
-        {item.previousOrders[0]}
-      </td>
-      <td className='border border-gray-700 text-center'>
-        <ChangeOrderAmount id={item.id} orderAmount={item.orderAmount} />
-      </td>
-      <td className='hidden md:table-cell border border-gray-700 text-center px-1'>
-        {item.supplier}
-      </td>
-      <td className='hidden md:table-cell border border-gray-700 text-center px-1'>
-        {item.location}
-      </td>
-    </tr>
+    <>
+      <tr>
+        <td className='hidden md:table-cell'>
+          <button onClick={() => setExpanded(!expanded)}>
+            {!expanded && <ClipboardDown />}
+            {expanded && <ClipboardUp />}
+          </button>
+        </td>
+        <td className='border border-gray-700 text-center px-1'>
+          {item.itemName}
+        </td>
+        <td className='border border-gray-700 text-center'>{item.buildTo}</td>
+        <td className='hidden md:table-cell border border-gray-700 text-center'>
+          {item.previousOrders[1]}
+        </td>
+        <td className='border border-gray-700 text-center'>
+          {item.previousOrders[0]}
+        </td>
+        <td className='border border-gray-700 text-center'>
+          <ChangeOrderAmount id={item.id} orderAmount={item.orderAmount} />
+        </td>
+        <td className='hidden md:table-cell border border-gray-700 text-center px-1'>
+          {item.supplier}
+        </td>
+        <td className='hidden md:table-cell border border-gray-700 text-center px-1'>
+          {item.location}
+        </td>
+      </tr>
+      {expanded && <TableItemRowExpanded item={item} />}
+    </>
   )
 }, areEqual)
 export default TableItemRow
