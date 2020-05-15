a659fb8cfe1603a13062bfd59a3216e1fb3272ba
JamesJ <1869237+shinygit@users.noreply.github.com>
2020-05-15 08:49:25 -0500

    not the way to do it

--- Backend/migrations/20200502181254-create-order.js
+++ Backend/migrations/20200502181254-create-order.js
@@ -26,9 +26,6 @@ module.exports = {
         allowNull: false,
         type: Sequelize.DATE,
       },
-      orderPlacedWithSupplier: {
-        type: Sequelize.ARRAY(Sequelize.INTEGER),
-      },
     })
   },
 
