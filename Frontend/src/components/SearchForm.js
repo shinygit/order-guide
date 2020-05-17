e36847f379798f5109cd74d0590bd78ef57dc82a
JamesJ <1869237+shinygit@users.noreply.github.com>
2020-05-17 10:32:35 -0500

    easier to find search bar

--- Frontend/src/components/SearchForm.js
+++ Frontend/src/components/SearchForm.js
@@ -5,19 +5,19 @@ import { FILTER_QUERY } from '../Queries/filter'
 const SearchForm = () => {
   const {
     data: {
-      filter: { searchTerm }
-    }
+      filter: { searchTerm },
+    },
   } = useQuery(FILTER_QUERY)
   const client = useApolloClient()
 
-  const handleChange = event => {
+  const handleChange = (event) => {
     client.writeData({
       data: {
         filter: {
           searchTerm: event.target.value,
-          __typename: 'Filter'
-        }
-      }
+          __typename: 'Filter',
+        },
+      },
     })
   }
   return (
@@ -25,13 +25,13 @@ const SearchForm = () => {
       <input
         id='searchField'
         className='bg-white focus:outline-none
-        focus:shadow-outline border border-gray-300
+        focus:shadow-outline border-2 border-gray-500
          rounded-lg py-2 px-4 block w-full lg:w-1/2 mb-1'
         type='text'
         placeholder='Search...'
         value={searchTerm}
         onChange={handleChange}
-        onKeyPress={event => {
+        onKeyPress={(event) => {
           if (event.key === 'Enter') {
             event.target.blur()
           }
