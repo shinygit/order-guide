bec2a477c69b9a25b98da050d971ee297c185dac
JamesJ <1869237+shinygit@users.noreply.github.com>
2020-05-18 07:44:20 -0500

    more intuitive search

--- Frontend/src/components/SearchForm.js
+++ Frontend/src/components/SearchForm.js
@@ -15,6 +15,8 @@ const SearchForm = () => {
       data: {
         filter: {
           searchTerm: event.target.value,
+          filterType: 'ALL',
+          filterName: 'ALL',
           __typename: 'Filter',
         },
       },
@@ -28,7 +30,7 @@ const SearchForm = () => {
         focus:shadow-outline border-2 border-gray-500
          rounded-lg py-2 px-4 block w-full lg:w-1/2 mb-1'
         type='text'
-        placeholder='Search...'
+        placeholder='Search... (will show hidden items)'
         value={searchTerm}
         onChange={handleChange}
         onKeyPress={(event) => {
