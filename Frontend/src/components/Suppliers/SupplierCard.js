a5e26f37e9754ce43c7b83ffe2f3e9d152674602
JamesJ <1869237+shinygit@users.noreply.github.com>
2020-05-18 21:00:15 -0500

    cleaned up supplier card

--- Frontend/src/components/Suppliers/SupplierCard.js
+++ Frontend/src/components/Suppliers/SupplierCard.js
@@ -2,23 +2,36 @@ import React from 'react'
 
 const SupplierCard = ({ supplier, selectedCard, setSelectedCard }) => {
   return (
-    <div className='w-6/12 max-w-sm'>
+    <div className='w-6/12 max-w-sm text-gray-900'>
       <div
         onClick={() => setSelectedCard(supplier.id)}
-        className={`flex flex-col border-4 rounded p-1 m-2 ${
+        className={`flex flex-col border-2 rounded p-2 m-2 ${
           selectedCard === supplier.id
-            ? 'bg-gray-200 border-gray-600'
-            : 'bg-white border-gray-300 hover:bg-gray-100 hover:shadow-lg'
+            ? 'bg-gray-200 border-blue-700'
+            : 'bg-white border-gray-300 shadow hover:bg-gray-100 hover:shadow-lg hover:border-blue-300'
         }`}
       >
-        <div className='text-3xl '>{supplier.supplierName}</div>
-        <div className=''>Office Phone: {supplier.officePhoneNumber}</div>
-        <div className='text-sm'>Delivery Day: {supplier.deliveryDay}</div>
-        <div className='text-xl flex flex-row-reverse'>
-          Sales Person: {supplier.salesPersonName}
+        <div className='text-2xl font-semibold self-center'>Sysco</div>
+
+        <div className='grid grid-cols-2'>
+          <div className='flex flex-col'>
+            <span className='text-gray-700 text-sm pr-2'>office phone:</span>
+            <span className='pl-4'>{supplier.officePhoneNumber}</span>
+          </div>
+          <div className='flex flex-col ml-10'>
+            <span className='text-gray-700 text-sm -ml-4'>sales person:</span>
+            <span className=''>{supplier.salesPersonName}</span>
+          </div>
         </div>
-        <div className='flex flex-row-reverse'>
-          Phone: {supplier.salesPersonPhoneNumber}
+        <div className='grid grid-cols-2'>
+          <div className='flex flex-col'>
+            <span className='text-gray-700 text-sm pr-2'>delivery day:</span>
+            <span className='pl-4'>{supplier.deliveryDay}</span>
+          </div>
+          <div className='flex flex-col ml-10'>
+            <span className='text-gray-700 text-sm -ml-4'>phone:</span>
+            <span className=''>{supplier.salesPersonPhoneNumber}</span>
+          </div>
         </div>
       </div>
     </div>
