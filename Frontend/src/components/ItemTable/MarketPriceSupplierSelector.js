e2c108642031bca6875ef0d1d23b48a0f470645c
JamesJ <1869237+shinygit@users.noreply.github.com>
2020-05-25 18:34:01 -0500

    supplier selector styling

--- Frontend/src/components/ItemTable/MarketPriceSupplierSelector.js
+++ Frontend/src/components/ItemTable/MarketPriceSupplierSelector.js
@@ -38,18 +38,35 @@ const MarketPriceSupplierSelector = ({ item }) => {
   }
   if (!loadingSuppliersData)
     return (
-      <select
-        name='supplierName'
-        value={selectedSupplier}
-        onChange={handleSupplierChange}
-      >
-        <option key='unset' value={null}></option>
-        {listOfSuppliers.map((supplierName) => (
-          <option key={supplierName} value={supplierName}>
-            {supplierName}
+      <div className='inline-block relative'>
+        <select
+          className='appearance-none w-full bg-white border border-gray-400 hover:border-gray-500 px-4 py-2 pr-8 rounded shadow focus:outline-none focus:shadow-outline'
+          name='supplierName'
+          value={selectedSupplier}
+          onChange={handleSupplierChange}
+        >
+          <option key='unset' value={null}></option>
+          <option key='Market Price' value='Market Price'>
+            Market Price
           </option>
-        ))}
-      </select>
+          {listOfSuppliers.flatMap((supplierName) =>
+            supplierName === 'Market Price' ? null : (
+              <option key={supplierName} value={supplierName}>
+                {supplierName}
+              </option>
+            )
+          )}
+        </select>
+        <div className='pointer-events-none absolute inset-y-0 right-0 flex items-center px-2 text-gray-700'>
+          <svg
+            className='fill-current h-4 w-4'
+            xmlns='http://www.w3.org/2000/svg'
+            viewBox='0 0 20 20'
+          >
+            <path d='M9.293 12.95l.707.707L15.657 8l-1.414-1.414L10 10.828 5.757 6.586 4.343 8z' />
+          </svg>
+        </div>
+      </div>
     )
   return null
 }
