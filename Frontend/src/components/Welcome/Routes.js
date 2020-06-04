df87160d485d9e033750af423b7a6f3505a099c0
JamesJ <1869237+shinygit@users.noreply.github.com>
2020-06-03 22:30:43 -0500

    added routes for receiver

--- Frontend/src/components/Welcome/Routes.js
+++ Frontend/src/components/Welcome/Routes.js
@@ -4,17 +4,28 @@ import Login from '../User/Login'
 import Register from '../User/Register'
 import MainSupplierView from '../Suppliers/MainSupplierView'
 import Manual from '../Manual'
+import ReceiverLogin from '../Receiving/ReceiverLogin'
+import ReceivingPage from '../Receiving/ReceivingPage/ReceivingPage'
+import { GET_ME } from '../../Queries/user'
 import { Switch, Route, useHistory } from 'react-router-dom'
+import { useQuery } from '@apollo/react-hooks'
 
 export default function Routes() {
+  const { data, loading, error } = useQuery(GET_ME)
   const history = useHistory()
-  const token = localStorage.getItem('token')
+  if (loading) return null
+  const { me = {} } = data
   if (
-    !token &&
+    me === null &&
     history.location.pathname !== '/login' &&
-    history.location.pathname !== '/register'
+    history.location.pathname !== '/register' &&
+    history.location.pathname !== '/receiverlogin'
   ) {
+    localStorage.clear()
     history.push('/login')
+  }
+  if (me !== null && me.__typename === 'Receiver') {
+    history.push('/receiving')
   }
   return (
     <Switch>
@@ -24,11 +35,17 @@ export default function Routes() {
       <Route path='/register'>
         <Register />
       </Route>
+      <Route path='/receiverlogin'>
+        <ReceiverLogin />
+      </Route>
       <Route path='/suppliers'>
         <MainSupplierView />
       </Route>
       <Route path='/manual'>
         <Manual />
+      </Route>
+      <Route path='/receiving'>
+        <ReceivingPage />
       </Route>
       <Route path='/'>
         <App />
