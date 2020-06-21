37c5de4589dc031635533fe17310b8fb40dfd6cc
JamesJ <1869237+shinygit@users.noreply.github.com>
2020-06-21 17:02:29 -0500

    back to fuse.js search

--- Frontend/src/components/ItemTable/ListItems.js
+++ Frontend/src/components/ItemTable/ListItems.js
@@ -3,7 +3,7 @@ import TableItemRow from './TableItemRow'
 import EditItemForm from './EditItemForm'
 import gql from 'graphql-tag'
 import { useQuery, useMutation } from '@apollo/react-hooks'
-import fuzzysort from 'fuzzysort'
+import Fuse from 'fuse.js'
 
 import { ORDER_DATES } from '../../Queries/order'
 import { FILTER_QUERY } from '../../Queries/filter'
@@ -40,13 +40,15 @@ const ListItems = ({ items }) => {
     uncheckedItems.current = []
   }
 
+  const fuse = new Fuse(items, {
+    keys: ['itemName'],
+    ignoreLocation: true,
+  })
+
   const filteredItems = items.filter((item) => {
     if (searchTerm.length > 1) {
-      const searchResults = fuzzysort.go(searchTerm, items, {
-        key: 'itemName',
-        limit: 10,
-      })
-      return searchResults.map((x, i) => searchResults[i].obj).includes(item)
+      const searchResults = fuse.search(searchTerm)
+      return searchResults.map((x, i) => searchResults[i].item).includes(item)
     }
     if (filterName === 'ALL' && filterType === 'ALL') return true
     if (
