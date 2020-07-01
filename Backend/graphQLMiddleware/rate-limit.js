bd1c35d2410f44415fc93e70b1a4fe86a8f44b17
JamesJ <1869237+shinygit@users.noreply.github.com>
2020-07-01 14:59:29 -0500

    rate limit graphql middleware

---
+++ Backend/graphQLMiddleware/rate-limit.js
@@ -0,0 +1,21 @@
+import redis from './../redis'
+import { skip } from 'graphql-resolvers'
+
+const TWO_HOUR = 60 * 60 * 2
+
+export const rateLimit = (limit = 5) => async (parent, args, { req }, info) => {
+  const key = `rate-limit:${info.fieldName}:${req.ip}`
+  const current = await redis.incrAsync(key)
+  await redis.expireAsync(key, TWO_HOUR)
+
+  if (current > limit) {
+    return { passwordError: 'Just stop trying...' }
+  }
+  return skip
+}
+
+export const clearRateLimit = async (req, info) => {
+  const key = `rate-limit:${info.fieldName}:${req.ip}`
+  console.log(key)
+  await redis.delAsync(key)
+}
